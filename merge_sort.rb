26e7d70fb8e851e5d99b43cf3eba04acf5f2cf77
sdc9600 <149299980+sdc9600@users.noreply.github.com>
2024-01-10 20:13:20 +1030

    Testing function return

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,36 +1,38 @@
 require 'pry-byebug'
 array = [3, 2, 1, 13, 8, 5, 0, 1]
+array_copy = array
 
-def merge_sort(array, start = 0, mid = (array.length / 2) - 1.floor, final = array.length - 1)
+def merge_sort(array, array_copy = array, start = 0, mid = (array.length / 2) - 1.floor, final = array.length - 1)
   #binding.pry
   if array.length > 1
-    merge_sort(array[start..mid])
-    merge_sort(array[mid + 1..final])
+   # binding.pry
+    left_array = array_copy[start..mid]
+    right_array = array_copy[mid + 1..final]
+    merge_sort(left_array)
+    merge_sort(right_array)
+    merge(left_array, right_array)
+    p array_copy
   end
-  merge(array)
 end
 
-def merge(array, start = 0, mid = (array.length / 2) - 1.floor, final = array.length - 1)
+def merge(array = [], left_array, right_array)
   i = 0
   j = 0
-  array_copy = []
-  left_array = array[start..mid]
-  right_array = array[mid+1..final]
- # binding.pry
-  until array_copy.length == left_array.length + right_array.length do
+  until array.length == left_array.length + right_array.length do
     if left_array[i] == nil
-      array_copy.append(right_array[j..right_array.length - 1]).flatten!
+      array.append(right_array[j..right_array.length - 1]).flatten!
     elsif right_array[j] == nil
-      array_copy.append(left_array[i..left_array.length - 1]).flatten!
+      array.append(left_array[i..left_array.length - 1]).flatten!
     elsif left_array[i] <= right_array[j]
-      array_copy.append(left_array[i])
+      array.append(left_array[i])
       i += 1
     elsif left_array[i] > right_array[j]
-      array_copy.append(right_array[j])
+      array.append(right_array[j])
       j += 1
     end
   end
-  p  "Merged array: #{array_copy}"
+  p array
+  return array
 end
 
 merge_sort(array)
